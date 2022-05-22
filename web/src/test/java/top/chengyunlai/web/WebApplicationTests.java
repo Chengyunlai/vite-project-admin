6d6b2da45e85a3e7ec24974db43c5b8434a88bc1
cyl <1257997441@qq.com>
2022-05-22 14:10:09 +0800

    使用Maven构建第一版以Spring为基础的后端

--- web/src/test/java/top/chengyunlai/web/WebApplicationTests.java
+++ web/src/test/java/top/chengyunlai/web/WebApplicationTests.java
@@ -1,12 +1,6 @@
 package top.chengyunlai.web;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class WebApplicationTests {
-
-    @Test
     void contextLoads() {
     }
 
